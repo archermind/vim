5ed0ae29dacd3ec30ff42e845683890c0257a28e
Alan Wang <alan_t@foxmail.com>
2016-04-01 13:28:13 +0800

    Amend cs.sh script

--- cs.sh
+++ cs.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-find . -name "*.h" -o -name "*.c" -o -name "*.cc" > cscope.files
+find . -name "*.h" -o -name "*.c" -o -name "*.cc" -o -name "*.cpp" > cscope.files
 cscope -bkq -i cscope.files
 ctags -R
